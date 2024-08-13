7aa822b4588124a7805c25c96c2bf9baf23b3681
diyaprajapati <diyabprajapati2005@gmail.com>
2024-08-13 23:17:43 +0530

    add toggle button

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -8,17 +8,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     EditText uname, pwd;
     Button loginBtn;
     SharedPreferences pref;
     Intent intent;
+    private ToggleButton themeToggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Load the theme before setting the content view
+        SharedPreferences preferences = getSharedPreferences("themePrefs", MODE_PRIVATE);
+        boolean isDarkTheme = preferences.getBoolean("isDarkTheme", false);
+        if (isDarkTheme) {
+            setTheme(R.style.AppTheme_Dark);
+        } else {
+            setTheme(R.style.AppTheme_Light);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        for toggle button
+        themeToggleButton = findViewById(R.id.themeToggleButton);
+        themeToggleButton.setChecked(isDarkTheme);
+        updateToggleButtonIcon(isDarkTheme);
+
+        themeToggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // Save the theme preference
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("isDarkTheme", isChecked);
+            editor.apply();
+
+            // Recreate the activity to apply the new theme
+            recreate();
+        });
+
+//        other login code
         uname = findViewById(R.id.txtName);
         pwd = findViewById(R.id.txtPwd);
         loginBtn = findViewById(R.id.btnLogin);
@@ -46,5 +75,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void updateToggleButtonIcon(boolean isDarkTheme) {
+        if (isDarkTheme) {
+            themeToggleButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_moon_foreground, 0, 0);
+        } else {
+            themeToggleButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_sun_foreground, 0, 0);
+        }
     }
 }
